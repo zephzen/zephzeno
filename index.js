0fc7c97b84e0bd6ff841926095872c279603c0bf
zephzen <luck@Luck-Pro.local>
2024-03-21 18:27:40 +0800

    add repository url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // useGenerateWallets.js
-const generateWallets = require('zoahn');
+const generateWallets = require('zephzen');
 
 // 使用第一个项目的功能
-generateWallets(3, 'matthewmed.xlsx');
+generateWallets(3, 'zz.xlsx');
